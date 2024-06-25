6de9638c2c4718cffb5b5b2cb9de6c8fee1d08ef
mohammadrad81 <mohammad.rad81@gmail.com>
2024-06-25 10:38:56 +0330

    previously implemented

--- preprocess/stop_words.py
+++ preprocess/stop_words.py
@@ -22,5 +22,5 @@ def is_stop_word(token: str) -> bool:
     return token in STOP_WORDS
 
 
-def remove_stop_words(tokens: list[str]):
+def remove_stop_words(tokens: list[str]) -> list[str]:
     return [token for token in tokens if token not in STOP_WORDS]
